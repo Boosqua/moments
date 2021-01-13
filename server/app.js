2b1bb820dd4aabc588c6a47f3663570885eea079
Omar Hernandez <oah232@gmail.com>
2021-01-13 04:07:33 -0800

    fix packaje.json

--- server/app.js
+++ server/app.js
@@ -18,6 +18,12 @@ app.set('view engine', 'html');
 app.use(passport.initialize());
 require("./config/passport")(passport);
 app.listen(process.env.PORT || 8080);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 app.get('/api', (req, res) => {
   res.send(`${new Date()}`);
 });

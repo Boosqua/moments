5dbe70340dae4f36f702f7433ea5d770d7ab5411
Omar Hernandez <oah232@gmail.com>
2021-01-13 06:30:31 -0800

    still deploying to heroku

--- db/index.js
+++ db/index.js
@@ -1,7 +1,12 @@
-const { Pool } = require("pg");
+const { Pool, Client } = require("pg");
 const { pgPool } = require("../config/keys")
 
-const pool = new Pool({connectionString: pgPool, SSL: true});
+const pool = new Client({
+  connectionString: pgPool,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 pool.query( "INSERT INTO users (username, password) VALUES (WORKING, MAYBE)" )
 module.exports = {
   query: (text, params, callback) => {

c13c21f5405bc3637fa37614e490bc4b3dbe994b
Omar Hernandez <oah232@gmail.com>
2021-01-13 03:30:41 -0800

    fix AWS bucket name

--- server/config/keys_prod.js
+++ server/config/keys_prod.js
@@ -1,6 +1,6 @@
 module.exports = {
   secretOrKey: process.env.SECRET_OR_KEY,
-  AWS_BUCKET_NAME: "pooch-dev",
+  AWS_BUCKET_NAME: "memories-ort",
   AWSaccessKeyId: process.env.AWS_ACCESS_KEY,
   secretAccessKey: process.env.SECRET_ACCESS_KEY,
   AWS_REGION: "us-west-1",

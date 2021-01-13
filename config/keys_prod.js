be2be9477b3bffd3a740f28bf47e4d68a54ea047
Omar Hernandez <oah232@gmail.com>
2021-01-13 05:46:14 -0800

    still deploying to heroku

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -4,8 +4,12 @@ module.exports = {
   AWSaccessKeyId: process.env.AWS_ACCESS_KEY,
   secretAccessKey: process.env.SECRET_ACCESS_KEY,
   AWS_REGION: "us-west-1",
-  pgPool: {
-     connectionString: process.env.DATABASE_URL,
-     ssl: true
-  }
+   pgPool: {
+      user: "jlkswqilvqnaqr",
+      host: "ec2-52-44-46-66.compute-1.amazonaws.com",
+      database: "d6mccecitvmvsj",
+      password:
+         "658d1785642ad7986fb683fb137642675b12657299d8ae2ca6693972301c7c27",
+      port: 5432,
+   },
 };

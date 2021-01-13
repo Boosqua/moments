30726538b7561a0a711e3b9ac16b7745b05cdc7d
Omar Hernandez <oah232@gmail.com>
2021-01-13 06:03:55 -0800

    still deploying to heroku

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 const { pgPool } = require("../config/keys")
 
-const pool = new Pool({pgPool.DATABASE_URL,});
+const pool = new Pool({pgPool});
 module.exports = {
   query: (text, params, callback) => {
     return pool.query(text, params, callback);
